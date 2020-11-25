3cfb8860bf723be15809b0dfb69ba067b6235a78
leyle <leyle@leyle.com>
2020-11-25 17:10:48 +0800

    update peer scripts

---
+++ peer/scripts/restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# import system wide env.sh
+./init.sh
+
+. ./env.sh
+
+# restart peer and cli
+# cannot restart ca
+# when restart peer, we need to rejoin the channel
+
+# todo
+
+
+
