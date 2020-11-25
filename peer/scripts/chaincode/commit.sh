11683082269556e8429e74a3c9ed97f33c3d1b08
leyle <leyle@leyle.com>
2020-11-25 23:15:49 +0800

    update peer chaincode scripts

--- peer/scripts/chaincode/commit.sh
+++ peer/scripts/chaincode/commit.sh
@@ -7,8 +7,6 @@
 # export go path
 export PATH=/usr/local/go/bin:$PATH
 
-cd $CC_SRC_PATH
-
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID=$ORG_MSPID
 export CORE_PEER_TLS_ROOTCERT_FILE=${FABRIC_CA_CLIENT_HOME}/peers/${PEER_NAME}/tls-msp/tlscacerts/tls-ca-cert.pem
@@ -19,10 +17,6 @@ export TLS_CA_FILE=${FABRIC_CA_CLIENT_HOME}/peers/${PEER_NAME}/tls-msp/tlscacert
 export ORDERER_HOST=orderer0.org0.fabric.test
 export ORDERER_HOSTPORT=orderer0.org0.fabric.test:7050
 
-peer lifecycle chaincode commit -o $ORDERER_HOSTPORT --ordererTLSHostnameOverride $ORDERER_HOST \ 
-    --channelID $APP_CHANNEL_NAME --name $CC_NAME --version $CC_PKG_VER --sequence $CC_SEQUENCE \
-    --tls true --cafile $TLS_CA_FILE \
-    --peerAddresses peer0.org1.fabric.test:7051 --tlsRootCertFiles $TLS_CA_FILE \
-    --peerAddresses peer0.org2.fabric.test:7051 --tlsRootCertFiles $TLS_CA_FILE
+peer lifecycle chaincode commit -o ${ORDERER_HOSTPORT} --ordererTLSHostnameOverride ${ORDERER_HOST} --channelID ${APP_CHANNEL_NAME} --name ${CC_NAME} --version ${CC_PKG_VER} --sequence ${CC_SEQUENCE} --tls true --cafile ${TLS_CA_FILE} --peerAddresses peer0.org1.fabric.test:7051 --tlsRootCertFiles ${TLS_CA_FILE} --peerAddresses peer0.org2.fabric.test:7051 --tlsRootCertFiles ${TLS_CA_FILE}
 
 peer lifecycle chaincode querycommitted --channelID $APP_CHANNEL_NAME --name $CC_NAME --tls true --cafile $TLS_CA_FILE
